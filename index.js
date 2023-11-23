f2647eba535c2966bf9046c7625334660c24c13a
calen_s <calenshirfield@gmail.com>
2023-11-23 13:01:15 +0000

    Project complete with suggested work list.

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const { parse } = require("./node_modules/csv-parse");
 const fs = require("fs");
 
-results = [];
+const habitablePlanets = [];
+
+function isHabitablePlanet(planet) {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+}
 
 fs.createReadStream("./planets/kepler_data.csv")
+  .pipe(
+    parse({
+      comment: "#",
+      columns: true,
+    })
+  )
   .on("data", (data) => {
-    results.push(data);
+    if (isHabitablePlanet(data)) {
+      habitablePlanets.push(data);
+    }
   })
   .on("error", (err) => {
     console.log("Error!");
@@ -13,7 +30,12 @@ fs.createReadStream("./planets/kepler_data.csv")
   })
   .on("end", () => {
     console.log("done");
-    console.log(results);
+    console.log(habitablePlanets.map((planet) => planet["kepler_name"]));
+    console.log(`${habitablePlanets.length} habitable planets found!`);
   });
 
-// parse();
+/*
+  TODO
+    1. Remove logs and add comments
+    2. Create a new csv file with only habitable planets
+   */
