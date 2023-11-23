c74a0d9a714bb827cbe2487e822fca7b729cdf2f
calen_s <calenshirfield@gmail.com>
2023-11-23 10:46:42 +0000

    Read stream with relevant emitters complete.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 const { parse } = require("./node_modules/csv-parse");
+const fs = require("fs");
+
+results = [];
+
+fs.createReadStream("./planets/kepler_data.csv")
+  .on("data", (data) => {
+    results.push(data);
+  })
+  .on("error", (err) => {
+    console.log("Error!");
+    console.log(err);
+  })
+  .on("end", () => {
+    console.log("done");
+    console.log(results);
+  });
+
+// parse();
